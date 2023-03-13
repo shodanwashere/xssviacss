4dcab791bfb61910477b0f0b74362ed1c0759f5f
Nuno Dias <jookaada@protonmail.com>
2023-03-13 15:30:18 +0000

    migrating to apache2

--- clean.sh
+++ clean.sh
@@ -4,3 +4,6 @@ echo »»»» STOPPED  »»»»
 echo ==== REMOVING ====
 docker rm `docker ps -a | grep xssviacss | cut -d' ' -f1`
 echo ==== REMOVED  ====
+echo «««« PRUNING  ««««
+docker system prune -a
+echo ««««  PRUNED  ««««
